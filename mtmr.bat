15f13e92f86aec17b00c28630c1b0dcd9b8b656c
Ivan Shirokov <laefree@yandex.ru>
2023-09-08 13:19:35 +0400

    untrack .idea files, modifed launch file (name myTimer -> mtmr; now it's don't close command line

---
+++ mtmr.bat
@@ -0,0 +1,2 @@
+@echo off
+start javaw -jar "C:\java education\timer\tymer-work-break\target\MyTimer-1.0.1-jar-with-dependencies.jar" %*
--- myTimer.bat
+++
@@ -1,3 +0,0 @@
-start javaw -jar C:\javaTimer\MyTimer\target\MyTimer-1.0-SNAPSHOT-jar-with-dependencies.jar
-timeout 2 > null
-exit
